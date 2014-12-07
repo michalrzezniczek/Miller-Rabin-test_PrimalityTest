cf81bdb2c7a3acbba549aa0a7bc4532464047b65
Michal Rzezniczek <michal.rzezniczek@outlook.com>
2014-12-07 23:38:05 +0100

    Add fast modular exponentiation

--- primalityTest.py
+++ primalityTest.py
@@ -10,3 +10,17 @@ def to_bin(dec_exponent):
         return [0]
     return bin_exponent
 
+
+def modularPower(dec_base, dec_exponent, modulo):
+    bin_exponent_reversed = to_bin(dec_exponent)
+    bin_exponent_reversed.reverse()
+    result = 1
+    dec_base %= modulo
+    tmp = dec_base
+    for bit in bin_exponent_reversed:
+        if bit != 0:
+            result *= tmp
+            result %= modulo
+        tmp *= tmp
+        tmp %= modulo
+    return result
